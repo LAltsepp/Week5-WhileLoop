9471028e6fd8d81c7d54c36808fd83b8f5ba05b4
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:46:04 +0300

    first commit

---
+++ PINValidationThreeTries/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PINValidationThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada pin koodi
+            //programm võrdleb pin koodi arvuga 1234
+            //kui sisestatud pin kood on 1234,
+            //programm kuvab kondoolis "tere tulemast"
+            //kui sisustatud pin kood on vale, programm kuvab konsooli,
+            //"vale pin proovi uuesti"
+            //katsete arv on piiramatu
+
+            Console.WriteLine("Sisesta oma PIN-kood");
+
+            int userPIN = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < 3)
+            if (userPIN == 1234)
+            {
+                Console.WriteLine("Norm PIN");
+                    break;
+            }
+            else
+            {
+                Console.WriteLine("Ei ole norm PIN");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada pin koodi
+            //programm võrdleb pin koodi arvuga 1234
+            //kui sisestatud pin kood on 1234,
+            //programm kuvab kondoolis "tere tulemast"
+            //kui sisustatud pin kood on vale, programm kuvab konsooli,
+            //"vale pin proovi uuesti"
+            //katsete arv on piiramatu
+
+            //boolean --> true/false
+            bool loopactive = true;
+
+            while (loopactive)
+            {
+                Console.WriteLine("Sisesta PIN-kood");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    loopactive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Vale PIN mees, proovi veel, ma usun sinusse!");
+                }
+            }
+
+
+            Console.WriteLine("Kena päeva!");
+            
+
+
+
+
+
+
+        }
+    }
+}
